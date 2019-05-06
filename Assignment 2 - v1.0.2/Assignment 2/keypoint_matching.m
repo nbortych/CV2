af2f2a34d27fb2b52757b1f859c492dea53869de
Michiel Bonnee <michiel.bonnee@gmail.com>
2019-05-06 22:16:27 +0200

    Somewhat finished version of part 3

--- Assignment 2 - v1.0.2/Assignment 2/keypoint_matching.m
+++ Assignment 2 - v1.0.2/Assignment 2/keypoint_matching.m
@@ -3,7 +3,7 @@ function keypoint_matches = keypoint_matching(im1, im2)
     [f2,d2] = vl_sift(im2);
     [matches, scores] = vl_ubcmatch(d1, d2);
     
-    matches1 = f1(1:2,matches(1,:));
-    matches2 = f2(1:2,matches(2,:));
+    matches1 = f1([1,2], matches(1,:));
+    matches2 = f2([1,2], matches(2,:));
     keypoint_matches = cat(1,matches1,matches2);
 end
