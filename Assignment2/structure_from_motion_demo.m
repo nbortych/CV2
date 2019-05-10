6bcb4b231201a8632be2dfe44cebd9c9e7da0341
alex-lindt <43879154+alex-lindt@users.noreply.github.com>
2019-05-10 20:22:34 +0200

    final_pvm

--- Assignment2/structure_from_motion_demo.m
+++ Assignment2/structure_from_motion_demo.m
@@ -3,7 +3,7 @@ block_size = 4;
 % Note: One of both tests needs to be commented out.
 
 %%%%%%%%% Test for generated point view matrix:
-load("42.mat");
+load("final_pvm.mat");
 point_view_generated(point_view_generated == 0) = nan;
 point_cloud_generated = structure_from_motion(point_view_generated,block_size);
 %%%%%%%%%
