fixed rotation

Assignment3/morphable_model.py
@@ -5,14 +5,8 @@ import h5py
 from data_def import Mesh
 from mesh_to_png import triangles, mean_tex, mesh_to_png
 
-def U(i=1):
-    """ 
-    Note: The higher you choose i, the further away the sampled face is from the mediocre face.
-    i=1 gives almost always the same face.
-    i=3 gives very expressive faces of various shape.
-    ...
-    """
-    return np.random.uniform(-i, i)
+def U(size):
+    return np.random.uniform(-1, 1, size=size)
 
 def read_pca_model(num_pc_id=30, num_pc_ex=20):
     bfm = h5py.File("model2017-1_face12_nomouth.h5", 'r')
@@ -30,23 +24,23 @@ def read_pca_model(num_pc_id=30, num_pc_ex=20):
     return {"mu_id": mu_id, "sigma_id": sigma_id, "E_id": E_id,
             "mu_ex": mu_ex, "sigma_ex": sigma_ex, "E_ex": E_ex}
 
-def sample_face(p, alpha, delta):
+def sample_face(p, num_pc_id=30, num_pc_ex=20):
     """
     p : PCA model received with read_pca_model()
     """
+    alpha = U(num_pc_id)
+    delta = U(num_pc_ex)
     G_id = p["mu_id"] + p["E_id"] @ ( p["sigma_id"] * alpha)
     G_ex = p["mu_ex"] + p["E_ex"] @ ( p["sigma_ex"] * delta)
-    return G_id+G_ex
+    return (G_id+G_ex).reshape((-1, 3))
 
-def sample_face_pointclouds(num_samples=24):
+def generate_face_images(num_samples=24):
     # read pca model from files
     pca_model = read_pca_model()
 
     for i in range(num_samples):
         # sample new face with given formula
-        alpha = U(i=1)
-        delta = U(i=1)
-        f_pc = sample_face(pca_model, alpha, delta).reshape((-1, 3)) # (3N, )= (85764,)
+        f_pc = sample_face(pca_model) # (3N, )= (85764,)
         
         # show mesh:
         # mesh = trimesh.base.Trimesh(vertices=f_pc, faces=triangles, vertex_colors=mean_tex)
@@ -55,5 +49,5 @@ def sample_face_pointclouds(num_samples=24):
         # save mesh
         mesh = Mesh(vertices=f_pc, colors=mean_tex, triangles=triangles)
         mesh_to_png(f"./results/morphable_model/{str(i)}.png", mesh)
-        
-sample_face_pointclouds()
+
+generate_face_images()

Assignment3/pinhole_camera_model.py
@@ -5,15 +5,40 @@ from perspective_projection_matrix import get_perspective, get_P
 from morphable_model import sample_face, read_pca_model, U
 from rotation_matrix import get_rotation_matrix
 
+from data_def import Mesh
+from mesh_to_png import triangles, mean_tex, mesh_to_png
 
-def pinhole_camera_model(alpha, delta, w, t):
+def rotate_face(angles):
+    """
+    :param angles: list of angles. each angle has three entries [theta_x, theta_y, theta_z]
+    :return:
+    """
+    # sample face
     pca = read_pca_model()
-    G = sample_face(pca, alpha, delta)
+    G = sample_face(pca).T
 
-    matrix = np.array([[G[0]],
-                        [G[1]],
-                        [G[2]],
-                        [1]])
+    # transform to homogeneous coordinates
+    G_h = np.append(G, np.ones(G.shape[1]).reshape((1, -1)), axis=0)
+
+    for w in angles:
+        # get rotation matrix
+        T = np.eye(4)
+        T[:3, :3] = get_rotation_matrix(w)
+
+        # save resulting rotated face
+        mesh = Mesh(vertices=(T @ G_h)[:3].T, colors=mean_tex, triangles=triangles)
+        mesh_to_png("./results/rotation/"+str(w)+".png", mesh)
+
+    return
+
+def pinhole_camera_model(w, t):
+
+    # sample face
+    pca = read_pca_model()
+    G = sample_face(pca).T
+
+    # transform to homogeneous coordinates
+    G_h = np.append(G, np.ones(G.shape[1]).reshape((1, -1)), axis=0)
 
     W = 255
     H = 255
@@ -30,19 +55,11 @@ def pinhole_camera_model(alpha, delta, w, t):
 
     PI = np.dot(V, P)
 
-    R = get_rotation_matrix(w)
+    return
 
-    T = np.array([[R[0][0], R[0][1], R[0][2], t[0]],
-        [R[1][0], R[1][1], R[1][2], t[1]],
-        [R[2][0], R[2][1], R[2][2], t[2]],
-        [0, 0, 0, 1]])
 
-    result = np.dot(PI, np.dot(T, matrix))
+# Task 3.1
+# Result are saved in results/rotation
+w = [[0,10,0], [0,0,0], [0,-10,0]]
+rotate_face(w)
 
-    print(result)
-
-alpha = U(i=1)
-delta = U(i=1)
-w = [10, 10, 10]
-t = [0, 0, 0]
-pinhole_camera_model(alpha, delta, w, t)